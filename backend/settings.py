b718d45fa0a1e43d3648633d8dc36e921f73bcce
Harigovind <harigovind@Harigovinds-MacBook-Pro.local>
2023-01-28 02:20:12 +0530

    converted to new db,deletes old migrations

--- backend/settings.py
+++ backend/settings.py
@@ -108,8 +108,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'orionlive',
+        'USER': 'adminlabglo',
+        'PASSWORD': 'harry007',
+        'HOST': '194.163.159.41',
+        'PORT': '5432',
     }
 }
 
