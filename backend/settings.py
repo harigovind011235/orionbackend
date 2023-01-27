f3afbfa19ba3ff549c67dec39fe6ed977ddc8270
Harigovind <harigovind@Harigovinds-MacBook-Pro.local>
2023-01-28 03:17:27 +0530

    changes user

--- backend/settings.py
+++ backend/settings.py
@@ -110,7 +110,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'orionlive',
-        'USER': 'postgres',
+        'USER': 'adminlabglo',
         'PASSWORD': 'harry007',
         'HOST': '127.0.0.1',
         'PORT': '5432',
