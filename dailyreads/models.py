04444ad323fc547bb1e44e66073703469c8cf698
Harigovind <harigovind@Harigovinds-MacBook-Pro.local>
2023-01-30 10:38:03 +0530

    make dailyreads model primarykey unique

--- dailyreads/models.py
+++ dailyreads/models.py
@@ -8,7 +8,7 @@ class Blogs(models.Model):
     title = models.CharField(max_length=200)
     short_description = models.TextField()
     target_link = models.CharField(max_length=200)
-    id = models.UUIDField(default=uuid.uuid4(),primary_key=True,editable=False)
+    id = models.UUIDField(default=uuid.uuid4(),primary_key=True,editable=False,unique=True)
     created = models.DateTimeField(auto_now_add=True,null=True)
 
     def __str__(self):
@@ -22,7 +22,7 @@ class News(models.Model):
     title = models.CharField(max_length=200)
     short_description = models.TextField()
     target_link = models.CharField(max_length=200)
-    id = models.UUIDField(default=uuid.uuid4(),primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4(),primary_key=True, editable=False,unique=True)
     created = models.DateTimeField(auto_now_add=True,null=True)
 
     def __str__(self):
