5cfcb1621e01826cc75c423073aa93ba5c94f4aa
saranya <bssaranya2919@gmail.com>
2023-02-06 23:20:53 +0530

    mailing functionality for leave approval

--- backend/settings.py
+++ backend/settings.py
@@ -150,6 +150,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'webmail.labglo.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
