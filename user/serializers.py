f0b3b316049e5267a5fc27ac8d21690a83f322fa
saranya <bssaranya2919@gmail.com>
2023-02-13 19:47:01 +0530

    update leave serializer

--- user/serializers.py
+++ user/serializers.py
@@ -30,8 +30,7 @@ class PendingLeaveSerializer(serializers.Serializer):
     employee_name = serializers.SerializerMethodField()
     employee_id = serializers.SerializerMethodField()
     count = serializers.SerializerMethodField()
-    employee_designation = serializers.SerializerMethodField()
-    employee_profile_image = serializers.SerializerMethodField()
+
 
     def get_employee_name(self, obj):
         return obj['employee__name']
@@ -42,14 +41,9 @@ class PendingLeaveSerializer(serializers.Serializer):
     def get_employee_id(self,obj):
         return obj['employee_id']
 
-    def get_employee_designation(self, obj):
-        return obj['employee__designation']
-    def get_employee_profile_image(self, obj):
-        return obj['employee__profile_image']
 
 class AllLeaveSerializer(serializers.ModelSerializer):
     employee_details = PendingLeaveSerializer(source='*')
-
     class Meta:
         model = Leave
         fields = '__all__'
